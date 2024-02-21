e863b49cde52e2fc4e095e03cdf5a1b2a4d21dee
n1417 <n1417@student.jamk.fi>
2024-02-21 14:53:24 +0200

    unicafe step5

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,31 +1,50 @@
 import { useState } from 'react'
 
-//button object
+//button component
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
+//statistic result lines
+const StatisticLine = ({ text, value }) => (
+  <div>
+    {text}: {value}
+  </div>
+);
+
 const Statistics = ({ good, neutral, bad }) => {
+  //count the statistics
   const total = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
   const percent = (good / total) * 100;
 
+  //show this text before feedback clicks have been given
+  if(total === 0){
+    return(
+      <p>No feedback given yet.</p>
+    )
+  }
   return (
     <div>
-      <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {percent} %</p>
+      <StatisticLine text="Good" value ={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="All" value={total} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positive percent" value={percent} />
     </div>
   );
 }
 
 const App = () => {
+  //props and handle clicks
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  //handling the button clicks
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
@@ -37,12 +56,6 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
-    </div>
-
-    <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
     </div>
 
     <h1>Statistics</h1>
